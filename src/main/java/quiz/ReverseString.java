39c0a61a32f062cab87757112bed0c3498572c2e
Dcosiem <david.cosiem@yahoo.com>
2017-12-20 10:07:07 -0500

    Add files via upload
    New Reverse method using my own implementation, works passing all 5 unit tests.

--- src/main/java/quiz/ReverseString.java
+++ src/main/java/quiz/ReverseString.java
@@ -2,6 +2,11 @@ package quiz;
 
 public class ReverseString {
     public String reverse(String input) {
-        return new StringBuffer(input).reverse().toString();
-    }
+        String answer = "";
+        for(int i = input.length() - 1; i >= 0; i--) {
+        	answer = answer.concat(String.valueOf(input.charAt(i)));
+        }
+        return answer;
+    }    
+    
 }
